ee576b3648195961548226ca2b5fabb5ab21c972
Ajay NS <ajay.ns@outlook.com>
2017-03-25 11:53:32 +0530

    Implement ng-soundmanager

--- assets/app.js
+++ assets/app.js
@@ -6,19 +6,25 @@ app.controller('appCtrl', function($scope) {
       id: 'one',
       title: 'one',
       artist: 'one',
+      album: 'one',
       url: 'http://www.schillmania.com/projects/soundmanager2/demo/_mp3/rain.mp3'
     },
     {
       id: 'two',
       title: 'two',
       artist: 'two',
+      album: 'one',
       url: 'http://www.schillmania.com/projects/soundmanager2/demo/_mp3/walking.mp3'
     },
     {
       id: 'three',
       title: 'three',
       artist: 'three',
+      album: 'ones',
       url: 'http://www.freshly-ground.com/misc/music/carl-3-barlp.mp3'
     }
   ]
+
+  $scope.isPlaying = false;
+  $scope.songHover = false;
 })
